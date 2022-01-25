12e03fa1c0d50cb6e568371ab349dc82cbb94788
vtrifunovic <76224191+vtrifunovic@users.noreply.github.com>
2022-01-25 15:39:02 -0600

    Add files via upload

--- javabackups/MainActivity.java
+++ javabackups/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
-
+import com.reactlibrary.RNReactLoggingPackage;
 import expo.modules.ReactActivityDelegateWrapper;
 
 public class MainActivity extends ReactActivity {

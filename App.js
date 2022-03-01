b547692a604a858690eb878e01bd967df4b8fe25
vtrifunovic <76224191+vtrifunovic@users.noreply.github.com>
2022-03-01 15:39:07 -0600

    Add files via upload

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Router from './src/navigation/router'
 import { StatusBar } from 'expo-status-bar';
 
+//initialization of app, starts on the mainmenu screen
 const App = () => {
   return (
     <>

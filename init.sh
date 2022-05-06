1da68932132488df09708976ff52e4126968ab55
vtrifunovic <76224191+vtrifunovic@users.noreply.github.com>
2022-05-05 19:09:13 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,18 +2,12 @@
 red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
-echo "Running Image Installation"
-echo "Checking python version"
-python3 --version
-echo "${green}Done${reset}"
+echo "Running Installation"
 sudo apt-get update
 echo "Installing python3"
 sudo apt-get install python3.8
 echo "${green}Done${reset}"
 sudo apt install python3-pip
-echo "${green}Done${reset}"
-echo "Checking pip version" 
-pip3 --version
 echo "${green}Done${reset}"
 echo "${red}Big${reset} installation"
 sudo apt-get install python3-pip build-essential git python3 python3-dev ffmpeg libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev zlib1g-dev
